ce8f3ee89f3d2d7ce3ba5f6f84f7586e174e9563
Shakib Jilani <64030706+Shakib40@users.noreply.github.com>
2024-01-06 01:29:32 +0530

    D

--- index.js
+++ index.js
@@ -3,9 +3,32 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = Index;
+exports.Input = void 0;
 var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-function Index() {
-  return /*#__PURE__*/_react["default"].createElement("div", null, "Index");
-}
+var Input = exports.Input = function Input() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, "Input");
+};
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Textarea = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var Textarea = exports.Textarea = function Textarea() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, "Textarea");
+};
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _Input = require("./hooks/Input");
+var _Textarea = require("./hooks/Textarea");
+var _default = exports["default"] = {
+  Input: _Input.Input,
+  Textarea: _Textarea.Textarea
+};
